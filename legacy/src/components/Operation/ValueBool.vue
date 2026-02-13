c2a4e72e8284fa63a93aa5937387e5d05ad4c135
ety001 <work@akawa.ink>
2026-02-14 04:42:38 +0800

    remove legacy code

--- legacy/src/components/Operation/ValueBool.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <span v-text="value" />
-</template>
-
-<script>
-export default {
-  props: ['value'],
-};
-</script>
--- legacy/src/components/Operation/ValueJson.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <pre class="mt-2 d-inline-block">{{ $filters.pretty(value) }}</pre>
-</template>
-
-<script>
-export default {
-  props: ['value'],
-};
-</script>
